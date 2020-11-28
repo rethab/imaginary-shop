6a5003a5ece7189a322c74137e01905f21c051c3
Reto Hablützel <rethab@protonmail.ch>
2020-11-28 10:35:56 +0100

    skeleton

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,49 @@
 <template>
 <v-app>
-  <v-navigation-drawer app>
-    <!-- -->
-  </v-navigation-drawer>
+  <v-app-bar
+   absolute
+   dark
+   shrink-on-scroll
+   prominent
+   scroll-target="#scrolling-techniques-3"
+   app>
 
-  <v-app-bar app>
-    <!-- -->
+
+      <v-toolbar-title>Imagine Shopping</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <router-link to="/cart">
+          <v-icon>mdi-cart-variant</v-icon>
+        </router-link>
+      </v-btn>
+
+      <template v-slot:extension>
+        <v-tabs align-with-title>
+          <v-tab>
+            <router-link to="/products/bedsheets">Bedsheets</router-link>
+          </v-tab>
+          <v-tab>
+            <router-link to="/products/cups">Cups</router-link>
+          </v-tab>
+          <v-tab>
+            <router-link to="/products/shoes">Shoes</router-link>
+          </v-tab>
+        </v-tabs>
+      </template>
   </v-app-bar>
 
-  <!-- Sizes your content based upon application components -->
   <v-main>
 
-    <!-- Provides the application the proper gutter -->
     <v-container fluid>
 
-      <!-- If using vue-router -->
       <router-view></router-view>
     </v-container>
   </v-main>
 
   <v-footer app>
-    <!-- -->
   </v-footer>
 </v-app>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld';
-
-export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
-};
-</script>
---
+++ src/router/index.js
@@ -0,0 +1,32 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Products from '../views/Products.vue'
+import Cart from '../views/Cart.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Products',
+    component: Products
+  },
+  {
+    path: '/prodcuts/:type',
+    name: 'Products',
+    component: Products
+  },
+  {
+    path: '/cart',
+    name: 'Cart',
+    component: Cart
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
