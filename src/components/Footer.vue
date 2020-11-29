5c02cde5f5a99dcd34e13d4be6cd612c2ad4485f
Reto Hablützel <rethab@protonmail.ch>
2020-11-29 12:08:57 +0100

    softer footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
   <template>
     <v-footer v-bind="localAttrs" :padless="padless" app >
-      <v-card dark flat tile width="100%" class="lighten-3 text-center">
+      <v-card flat tile width="100%" color="grey" class="darken-3 text-center">
 
         <v-card-text class="white--text">
           {{ new Date().getFullYear() }} — <strong>Lau's Shop</strong>
